d6246ac0d1f3d25a656bde88d19159f8ee7e821a
jarek <word.press@outlook.com>
2016-09-06 19:02:57 +0200

    setup

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,14 @@ var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
 
+
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
 
     browserSync.init({
         server: "./app"
-    });
+    }
+);
 
     gulp.watch("app/scss/*.scss", ['sass']);
     gulp.watch("app/*.html").on('change', browserSync.reload);
