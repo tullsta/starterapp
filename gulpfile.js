3779f03a58157f3a30fb1a661ebc2d90aab034f4
jarek <word.press@outlook.com>
2016-09-09 19:51:39 +0200

    Added gulp packages: scss, imagemin, plumber, uglify, autoprefixer. Changes in catalog tree

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,53 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
-var sass        = require('gulp-sass');
+var  gulp        = require('gulp'),
+        browserSync = require('browser-sync').create(),
+        sass        = require('gulp-sass'),
+        uglify = require('gulp-uglify'),
+        autoprefixer = require('gulp-autoprefixer'),
+        imagemin = require('gulp-imagemin'),
+        plumber = require('gulp-plumber');
 
-
-// Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
-
+// Static Server + watching scss/html/js files
+gulp.task('server', ['sass'], function() {
     browserSync.init({
-        server: "./app"
+        server: "./"
     }
 );
-
     gulp.watch("app/scss/*.scss", ['sass']);
-    gulp.watch("app/*.html").on('change', browserSync.reload);
+    gulp.watch("app/*.js", ['jsUglify']);
+    gulp.watch("app/img/*", ['imagemin']);
+    gulp.watch("*.html; build/css/*.css").on('change', browserSync.reload);
 });
 
-// Compile sass into CSS & auto-inject into browsers
+// compile sass into CSS & auto-inject into browser
 gulp.task('sass', function() {
     return gulp.src("app/scss/*.scss")
+        .pipe(plumber())
         .pipe(sass())
-        .pipe(gulp.dest("app/css"))
+//css autoprefixer
+            .pipe(autoprefixer({
+                browsers: ['last 2 versions'],
+            }))
+            .pipe(gulp.dest('build/css'))
         .pipe(browserSync.stream());
 });
 
-gulp.task('default', ['serve']);
+//uglify JS files & auto-inject into browser
+gulp.task('jsUglify', function() {
+    gulp.src("app/*.js")
+    .pipe(plumber())
+    .pipe(uglify())
+    .pipe(gulp.dest('build/js'))
+    .pipe(browserSync.stream());
+});
+
+//image compression
+gulp.task('imagemin', function() {
+    gulp.src("app/img/*")
+    .pipe(plumber())
+    .pipe(imagemin())
+    .pipe(gulp.dest('build/img'))
+    .pipe(browserSync.stream());
+});
+
+//default task server
+gulp.task('default', ['server']);
